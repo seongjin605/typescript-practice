1314ab430dceb96802275cc5bcfd9e07f94404ed
seongjin605 <qkrtjdwls605@naver.com>
2019-04-12 00:07:04 +0900

    start

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let isDone = false;
+console.log("isDone => ", isDone);
+let decimal = 6;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+console.log(decimal, hex, binary, octal);
+let color = "test";
+console.log("color =>", color);
+let fullName = `Bob Bobbington`;
+let age = 37;
+let sentence = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let isDone: boolean = false;
+console.log("isDone => ", isDone);
+
+let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+console.log(decimal, hex, binary, octal);
+
+let color: string = "test";
+console.log("color =>", color);
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next month.`;
+
+console.log(sentence);
